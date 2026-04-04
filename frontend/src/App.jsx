c2de84e2c32fae3fba6f91f84ec0067c35ca953d
Sangam Shinde <sangampshinde@gmail.com>
2026-04-05 00:15:50 +0530

    todays frontedn code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,54 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { Navigate, Route, Routes } from "react-router-dom"; 
+
 import './App.css'
+import FloatingShape from './components/FloatingShape.jsx'
+import SignUpPage from './pages/SignUpPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import EmailVerificationPage from './pages/EmailVerificationPage.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
    <>
-   <h1 class="text-3xl font-bold underline text-red-500">
-    Hello world!
-  </h1>
+  <div
+		className='min-h-screen bg-gradient-to-br
+    from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden'
+		>
+
+      <FloatingShape color='bg-green-500' size='w-64 h-64' top='-5%' left='10%' delay={0} />
+
+      <FloatingShape color='bg-blue-500' size='w-48 h-48' top='70%' left='80%' delay={5} />
+
+      <FloatingShape color='bg-purple-500' size='w-32 h-32' top='40%' left='-10%' delay={2} />
+
+      <Routes>
+        <Route
+        path="/"
+        element={"home"}
+        />
+
+         <Route
+        path="/signup"
+        element={<SignUpPage/>}
+        />
+
+         <Route
+        path="/login"
+        element={<LoginPage/>}
+        />
+
+        <Route path='/verify-email' element={<EmailVerificationPage />} />
+
+        
+
+
+
+      </Routes>
+
+
+    </div>
    
    
    
---
+++ frontend/src/store/authStore.js
@@ -0,0 +1,5 @@
+import { create } from "zustand";
+import axios from "axios";
+
+
+
