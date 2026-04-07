5d0eceb17348918c9d3f61a9d40b9825621cc3d9
Sangam Shinde <sangampshinde@gmail.com>
2026-04-07 20:32:15 +0530

    connected online database

--- backend/data-source.js
+++ backend/data-source.js
@@ -1,15 +1,36 @@
+// import "reflect-metadata";
+// import { DataSource } from "typeorm";
+// import { User } from "./entities/User.entity.js";
+
+
+// const AppDataSource = new DataSource({
+//   type: "postgres",
+//   host: "localhost",
+//   port: 5432,
+//   username: "postgres",
+//   password: "Admin@123",
+//   database: "auth_system_db",
+//   synchronize: true,
+//   logging: false,
+//   entities: [User],
+// });
+
+// export default AppDataSource;
+
+
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { User } from "./entities/User.entity.js";
+import dotenv from "dotenv";
 
+dotenv.config();
 
 const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "Admin@123",
-  database: "auth_system_db",
+  url: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   synchronize: true,
   logging: false,
   entities: [User],
