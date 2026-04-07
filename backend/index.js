12b9dd776d9374d976c6aded2b9994b24fbcb071
Sangam Shinde <sangampshinde@gmail.com>
2026-04-07 20:40:07 +0530

    deployment issue resolsved

--- backend/index.js
+++ backend/index.js
@@ -31,11 +31,13 @@ app.use("/api/auth", authRoutes);
 
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	const frontendPath = path.join(__dirname, "frontend", "dist");
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+	app.use(express.static(frontendPath));
+
+	app.get("/*", (req, res) => {
+	res.sendFile(path.join(frontendPath, "index.html"));
+});
 }
 
 
